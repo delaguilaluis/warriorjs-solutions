1a78e95bff1922eb69f4c2e393849fcd314c40ba
Luis Del Aguila <luisdelaguila@outlook.com>
2016-07-01 22:33:13 -0600

    Level 2

--- luis-intermediate/Player.js
+++ luis-intermediate/Player.js
@@ -1,5 +1,27 @@
-class Player {
+class Player { // eslint-disable-line no-unused-vars
   playTurn(warrior) {
-    warrior.walk(warrior.directionOfStairs());
+    // Gather information about surroundings
+    const directions = ['forward', 'backward', 'right', 'left'];
+    const surroundings = directions.map((direction) => (
+      {
+        direction,
+        space: warrior.feel(direction),
+      }
+    ));
+
+    const spaceWithEnemy = surroundings.find((surrounding) => (
+      surrounding.space.isEnemy()
+    ));
+
+    if (spaceWithEnemy) {
+      return warrior.attack(spaceWithEnemy.direction);
+    }
+
+    // You are dying! Rest a bit!
+    if (warrior.health() < 15) {
+      return warrior.rest();
+    }
+
+    return warrior.walk(warrior.directionOfStairs());
   }
 }
